56d84bd530acd3610a8a4aa87819ad0fae2bec72
Rummonislam786 <islam.rummon786@gmail.com>
2022-08-20 12:38:08 +0600

    Add project files.

---
+++ rdlc_report_CB/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Reporting.WinForms;
+namespace rdlc_report_CB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Window> Windows { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Windows = new List<Window> 
+            { 
+                new Window { Name = "UsersWindow" },
+                new Window { Name = "SettingsWindow"  }, 
+                new Window { Name = "SettingsWindow" }
+            };
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = (ComboBox)e.Source;
+
+            if (comboBox.SelectedItem == null) return;
+
+            var selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            var window = Windows.FirstOrDefault(w => w.Name.Equals(selectedItem.Tag));
+
+            if (window == null) return;
+            window.ShowDialog();
+        }
+    }
+}
---
+++ rdlc_report_CB/newwindow1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rdlc_report_CB
+{
+    /// <summary>
+    /// Interaction logic for newwindow1.xaml
+    /// </summary>
+    public partial class newwindow1 : Window
+    {
+        public newwindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
